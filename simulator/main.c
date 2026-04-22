0c00ec0c8f3936a2e26188599e70a99ad7562a4c
PalindromicBreadLoaf <herbthehaircut@proton.me>
2026-04-22 15:48:00 -0400

    Update main supporting input

--- simulator/main.c
+++ simulator/main.c
@@ -1,10 +1,13 @@
 #include <SDL2/SDL.h>
 #include "../display.h"
+#include "../input.h"
+#include "input_sim.h"
 
 extern void display_sim_quit(void);
 
 int main(void) {
     display_init();
+    input_init();
 
     /* Draw a test pattern: horizontal rainbow gradient top-to-bottom,
        with brightness increasing left-to-right, so every panel is visible. */
@@ -18,13 +21,9 @@ int main(void) {
     }
     display_show();
 
-    SDL_Event event;
-    int running = 1;
-    while (running) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) running = 0;
-            if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_q) running = 0;
-        }
+    while (!input_sim_should_quit()) {
+        input_poll();
+        /* Game loop will go here. */
         SDL_Delay(16);
     }
 
