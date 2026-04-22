a677f66b0c3bc94d550c166b8b2df4454e206d0a
PalindromicBreadLoaf <herbthehaircut@proton.me>
2026-04-22 16:11:43 -0400

    Add entrypoint for Conway's Game of Life

--- simulator/main.c
+++ simulator/main.c
@@ -1,29 +1,25 @@
 #include <SDL2/SDL.h>
+#include <stdlib.h>
+#include <time.h>
 #include "../display.h"
 #include "../input.h"
+#include "../conway.h"
 #include "input_sim.h"
 
 extern void display_sim_quit(void);
 
 int main(void) {
+    srand((unsigned)time(NULL));
+
     display_init();
     input_init();
-
-    /* Draw a test pattern: horizontal rainbow gradient top-to-bottom,
-       with brightness increasing left-to-right, so every panel is visible. */
-    for (int y = 0; y < DISPLAY_H; y++) {
-        for (int x = 0; x < DISPLAY_W; x++) {
-            uint8_t r = (uint8_t)((x * 255) / (DISPLAY_W - 1));
-            uint8_t g = (uint8_t)((y * 255) / (DISPLAY_H - 1));
-            uint8_t b = 80;
-            display_set((uint8_t)x, (uint8_t)y, r, g, b);
-        }
-    }
-    display_show();
+    conway_init();
 
     while (!input_sim_should_quit()) {
-        input_poll();
-        /* Game loop will go here. */
+        InputEvent ev = input_poll();
+        conway_on_input(ev);
+        conway_update((unsigned long)SDL_GetTicks());
+        display_show();
         SDL_Delay(16);
     }
 
